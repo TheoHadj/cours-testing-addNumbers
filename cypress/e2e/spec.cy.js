1cf4a580b82b6a722843c54e736593e2c4e7636b
Theo Hadj <theo.hadja@gmail.com>
2024-10-31 11:51:34 +0100

    comments all, add poke api

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,64 +1,99 @@
-describe(`addingNumbers's spec`, () => {
+// describe(`addingNumbers's spec`, () => {
+
+//   it(`should load app`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//   })
+
+//   it(`should add numbers`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#firstNumber").type(5);
+//     cy.get("#secondNumber").type(5);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("10");
+//   })
+
+//   it(`should add negative numbers`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#firstNumber").type(-8);
+//     cy.get("#secondNumber").type(-3);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("-11");
+//   })
+
+//   it(`should works with decimals`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#firstNumber").type(1.1);
+//     cy.get("#secondNumber").type(2.2);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("3.3");
+//   })
+
+//   it(`should works while adding 0s`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#firstNumber").type(0);
+//     cy.get("#secondNumber").type(0);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("0");
+//   })
+
+//   it(`should not accept typing char`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#firstNumber").type("a");
+//     cy.get("#firstNumber").type(1);
+//     cy.get("#secondNumber").type(1);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("2");
+//   })
+
+  
+//   it(`should works living one input empty`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#secondNumber").type(1);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("1");
+//   })
+
+  
+//   it(`result should be NaN if one input empty`, () => {
+//     cy.visit("http://127.0.0.1:5173")
+//     cy.get("#secondNumber").type(1);
+//     cy.get("#calculBtn").click()
+//     cy.get("#result").contains("NaN");
+//   })
+
+// })
+
+
+
+
+
+
+
+
+describe(`PokemonApi's spec`, () => {
+
+  beforeEach(() =>{cy.visit("http://127.0.0.1:5173")})
 
   it(`should load app`, () => {
-    cy.visit("http://127.0.0.1:5173")
-  })
+  });
 
-  it(`should add numbers`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#firstNumber").type(5);
-    cy.get("#secondNumber").type(5);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("10");
-  })
+  it(`should get 200 from API`, () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon').should((response) => {
+      expect(response.status).to.eq(200);
+    });
 
-  it(`should add negative numbers`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#firstNumber").type(-8);
-    cy.get("#secondNumber").type(-3);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("-11");
-  })
+  });
 
-  it(`should works with decimals`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#firstNumber").type(1.1);
-    cy.get("#secondNumber").type(2.2);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("3.3");
-  })
+  it(`body from API`, () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon').should((response) => {
+      expect(response.body.results.length).to.be.greaterThan(1);
+    });
+  });
 
-  it(`should works while adding 0s`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#firstNumber").type(0);
-    cy.get("#secondNumber").type(0);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("0");
-  })
-
-  it(`should not accept typing char`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#firstNumber").type("a");
-    cy.get("#firstNumber").type(1);
-    cy.get("#secondNumber").type(1);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("2");
-  })
-
-  
-  it(`should works living one input empty`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#secondNumber").type(1);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("1");
-  })
-
-  
-  it(`result should be NaN if one input empty`, () => {
-    cy.visit("http://127.0.0.1:5173")
-    cy.get("#secondNumber").type(1);
-    cy.get("#calculBtn").click()
-    cy.get("#result").contains("NaN");
-  })
+  it(`duration shoulbe be <2000 from API`, () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon').should((response) => {
+      expect(response.duration).to.be.lessThan(2000);
+    });
+  });
 
 })
--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 import './style.css'
 // import javascriptLogo from './javascript.svg'
 // import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import { setupCounter } from './counter.js'
 
-setupCounter(document.querySelector('#counter'));
+// setupCounter(document.querySelector('#counter'));
 
-function addition(num1, num2) {
-  return num1 + num2;
-}
+// function addition(num1, num2) {
+//   return num1 + num2;
+// }
 
-function calculate() {
-  let firstNumber = parseFloat(document.getElementById('firstNumber').value);
-  let secondNumber = parseFloat(document.getElementById('secondNumber').value);
+// function calculate() {
+//   let firstNumber = parseFloat(document.getElementById('firstNumber').value);
+//   let secondNumber = parseFloat(document.getElementById('secondNumber').value);
 
-  let result = addition(firstNumber, secondNumber);
+//   let result = addition(firstNumber, secondNumber);
 
-  document.getElementById('result').innerText = result;
-}
-let btn = document.querySelector('button')
-btn.addEventListener('click',calculate)
+//   document.getElementById('result').innerText = result;
+// }
+// let btn = document.querySelector('button')
+// btn.addEventListener('click',calculate)
 
 
-
+const pokemonListe = document.getElementById('pokeListe');
+console.log(pokemonListe);
+const pokemonApiContact = async () => {
+    const pokemonData = await fetch('https://pokeapi.co/api/v2/pokemon');
+    console.log('pokemonData',pokemonData);
+    const pokemonDataTransformed = await pokemonData.json();
+    console.log('pokemonDataTransformed',pokemonDataTransformed);
+    console.log(pokemonDataTransformed.results[0].name);
+    for(let i=0;i<pokemonDataTransformed.results.length;i++){
+        let listElement = document.createElement('p');
+        listElement.innerText = pokemonDataTransformed.results[i].name;
+        pokemonListe.append(listElement);
+    }
+};
+pokemonApiContact();
 
